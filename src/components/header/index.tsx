4dbc45e93ed729ecb849c5379316fd93fb4dabb7
Castellian <sophiequines@gmail.com>
2022-09-21 21:19:36 -0300

    Finishing style of header and updating types

---
+++ src/components/header/index.tsx
@@ -0,0 +1,15 @@
+import * as S from "./style";
+
+export const Header: React.FC = () => {
+  return (
+    <S.Headers>
+      <S.LinksWrapper>
+        <S.Links>Home</S.Links>
+        <S.Links>Matrículas</S.Links>
+        <S.Links>Calendário</S.Links>
+        <S.Links>Preços</S.Links>
+        <S.Links>Sobre</S.Links>
+      </S.LinksWrapper>
+    </S.Headers>
+  );
+};
---
+++ src/components/header/style.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Headers = styled.header`
+  height: 80px;
+  background-color: var(--white);
+  display: flex;
+  place-items: space-between;
+  place-content: end;
+`;
+
+export const LinksWrapper = styled.div`
+  display: flex;
+  place-content: space-between;
+  width: 400px;
+  padding: 24px 240px 0 0;
+`;
+
+export const Links = styled.a`
+  color: var(--black);
+  line-height: 50px;
+  text-align: center;
+  cursor: pointer;
+
+  transition: 0.4s;
+
+  &:hover {
+    color: var(--pink);
+  }
+`;
